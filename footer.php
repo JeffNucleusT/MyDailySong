162d70e686c491f1e5ac317cde6a745ab859badf
Jeff Ngnoulaye <ngnoulayejeff2@gmail.com>
2018-04-11 13:23:27 +0100

    add Privacy and Terms links

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
         <footer class="page-footer">
             <div class="row justify-content-center">
-                <div class="col-12 align-self-start social-media">
+                <div class="col-12 align-self-start social-media mb-4">
                     <a href="https://www.facebook.com/My-Daily-Song-633052103521565" data-toggle="tooltip" data-placement="top" title="Facebook" target="_blank"><i class="icon-facebook"></i></a>
                     <a href="#" data-toggle="tooltip" data-placement="top" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
                     <a href="#" data-toggle="tooltip" data-placement="top" title="Google+" target="_blank"><i class="icon-google-plus-circle"></i></a>
                     <a href="https://chat.whatsapp.com/L2nlNrq6jtqDRFg3JtUIUI" data-toggle="tooltip" data-placement="top" title="Whatsapp" target="_blank"><i class="icon-whatsapp"></i></a>
                 </div>
-                <div class="col-12 align-self-end">@ 2018 My Daily Song <i class="icon-record"></i> All rights reserved | Developed by <a href="https://www.nucleus-technologies.com/" target="_blank">Nucléus Technologies</a>.</div>
+                <div class="col-12 align-self-end mt-4">
+                    @ 2018 My Daily Song <i class="icon-record"></i> All rights reserved | Developed by <a href="https://www.nucleus-technologies.com/" target="_blank">Nucléus Technologies</a>.
+                    <br><br> <a href="./?page=privacy">Privacy Concerns</a> <i class="icon-record"></i> <a href="./?page=terms">Terms and Conditions</a>
+                </div>
             </div>
             <button type="button" id="toTop"><i class="icon-up-circle-1"></i></button>
         </footer>
